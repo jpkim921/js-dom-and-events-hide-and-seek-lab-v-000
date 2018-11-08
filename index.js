497ce3079de0672155371c9493246a950a5ea095
Jayon Kim <jpkim921@gmail.com>
2018-11-08 03:37:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget () {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild () {
+  const nodes =  document.querySelectorAll('div#grand-node div')
+  return nodes[nodes.length - 1]
+}
+
+function increaseRankBy (n) {
+  const nodes = document.querySelectorAll('.ranked-list')
+  console.log(nodes)
+}
