68d4930ba37cbc645c7b95b45e82db7458dcc037
Jayon Kim <jpkim921@gmail.com>
2018-11-08 04:03:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget () {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild () {
+  const nodes =  document.querySelectorAll('div#grand-node div')
+  return nodes[nodes.length - 1]
+}
+
+function increaseRankBy(n) {
+  const nodes = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = nodes.length; i < l; i++) {
+    let children = nodes[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
