6142157182d24763bb4a7823d36be0b56ac7250f
Jayon Kim <jpkim921@gmail.com>
2018-11-08 04:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ function deepestChild () {
   return nodes[nodes.length - 1]
 }
 
-function increaseRankBy (n) {
-  const nodes = document.querySelectorAll('.ranked-list')
-}
+// function increaseRankBy (n) {
+//   const nodes = document.querySelectorAll('.ranked-list')
+// }
 
 
 function increaseRankBy(n) {
   const nodes = document.querySelectorAll('.ranked-list')
 
   for (let i = 0, i < nodes.length; i++) {
-    let children = rankedLists[i].children
+    let children = nodes[i].children
 
     for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n
